c295c68672e49a068e0087504f3eae306c1a2c54
John <Johney525@Hotmail.com>
2020-07-11 02:29:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function startGame(){
+  myGameArea.start();
+  myGamePeice = new component(30,30, "red", 10, 20)
+}
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
+
+function component(width, height, color, x, y){
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.x = x;
+  this.y = y;
+
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width,this.height);
+
+}
+startGame()
